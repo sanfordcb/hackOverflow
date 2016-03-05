6763ff060e37127afbccb81bac0abb5b56f6ce5d
Brian Moore <bmoorebrian53@gmail.com>
2016-03-05 15:41:50 -0500

    Update add-post.js

--- client/app/posts/add-post.js
+++ client/app/posts/add-post.js
@@ -24,13 +24,8 @@ angular.module('hackoverflow.add-post', [
 
   $scope.submit = function () {
     Posts.createPost($scope.title, $scope.body, $scope.forum,
-<<<<<<< HEAD
-      $rootScope.user, new Date(), $scope.tags);
-    $state.go('forums.posts', { 'forum': $scope.forum });
-=======
       $rootScope.user, new Date(), $scope.tags, $scope.votes);
     $state.go('posts', { 'forum': $scope.forum });
->>>>>>> 4e2f03888eb4e8fae2400c2e4c8020e8790f9a3e
   };
 
   $scope.getForums();
